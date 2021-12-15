54f3cf58858a3c537a3864045252d01eb5186d86
Rolso22 <ramagf@yandex.ru>
2021-12-15 21:54:59 +0300

    loopees

--- src/main/java/Loop.java
+++ src/main/java/Loop.java
@@ -2,33 +2,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Loop {
-    private static ArrayList<String> checked = new ArrayList<>();
-
     public static boolean isLoop(TRS trs) {
         ArrayList<Rule> allRules = trs.getRules();
         for (Rule r : allRules) {
-            String rulePair = r.getLefTerm()+"-"+r.getRightTerm();
-            if (!checked.contains(rulePair)) {
-                if (isUnified(r, trs))
-                    return true;
-            }
+            ArrayList<String> checked = new ArrayList<>();
+            if (isUnified(r, trs, checked))
+                return true;
         }
         return false;
     }
 
-    private static boolean isUnified(Rule r, TRS trs) {
-        checked.add(r.getLefTerm()+"-"+r.getRightTerm());
+    private static boolean isUnified(Rule r, TRS trs, ArrayList<String> checked) {
+        System.out.println(r.getLefTerm().getTermName() + " - " + r.getRightTerm().getTermName());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        checked.add(r.getLefTerm() + "-" + r.getRightTerm());
         boolean res = false;
-        trs.getRules();
         ArrayList<Rule> allRules = trs.getRules();
+        for (Rule rl : trs.getRules()) {
+            System.out.println("rl1 " + rl.getLefTerm().getTermName() + " - " + rl.getRightTerm().getTermName());
+        }
         for (Rule rl : allRules) {
+            System.out.println("rl " + rl.getLefTerm().getTermName() + " - " + rl.getRightTerm().getTermName());
             HashMap<String, Term> unSubst = new HashMap();
             unSubst = Unifier.unify(r.getRightTerm(), rl.getLefTerm(), unSubst);
+            //unSubst.forEach((key, value) -> System.out.println(value.getTermName()));
+            System.out.println(unSubst);
             if (unSubst == null) {
                 res = true;
-                String rulePair = r.getLefTerm()+"-"+r.getRightTerm();
-                if (!checked.contains(rulePair)) {
-                    if (!isUnified(rl, trs))
+                String rulePair = rl.getLefTerm() + "-" + rl.getRightTerm();
+                if (checked.contains(rulePair)) {
+                    if (!isUnified(rl, trs, checked))
                         return false;
                 }
             }
--- src/main/java/Unifier.java
+++ src/main/java/Unifier.java
@@ -9,11 +9,11 @@ public class Unifier {
             return null;
         } else if (x.equals(y)) {
             return unSubst;
-        } else if (Term.isVariable(x)) {
+        } else if (x.getTermType() == Term.TermType.VARIABLE) {
             return unifyVariable(x, y, unSubst);
-        } else if (Term.isVariable(y)) {
+        } else if (y.getTermType() == Term.TermType.VARIABLE) {
             return unifyVariable(y, x, unSubst);
-        } else if (Term.isConstructor(x) && Term.isConstructor(y)) {
+        } else if (x.getTermType() == Term.TermType.FUNCTION && y.getTermType() == Term.TermType.FUNCTION) {
             if (x.getTermName().equals(y.getTermName()) && x.getArguments().size() == y.getArguments().size()) {
                 for(int i = 0; i < x.getArguments().size(); ++i) {
                     unSubst = unify((Term)x.getArguments().get(i), (Term)y.getArguments().get(i), unSubst);
@@ -36,12 +36,12 @@ public class Unifier {
         } else if (!unSubst.containsKey(variable.getTermName())) {
             unSubst.put(variable.getTermName(), y);
             return unSubst;
-        } else if (Term.isVariable(y) && !unSubst.containsKey(y.getTermName())) {
+        } else if (y.getTermType() == Term.TermType.VARIABLE && !unSubst.containsKey(y.getTermName())) {
             unSubst.put(y.getTermName(), variable);
             return unSubst;
         } else if (unSubst.containsKey(variable.getTermName())) {
             return unify((Term)unSubst.get(variable.getTermName()), y, unSubst);
-        } else if (Term.isVariable(y) && unSubst.containsKey(y.getTermName())) {
+        } else if (y.getTermType() == Term.TermType.VARIABLE && unSubst.containsKey(y.getTermName())) {
             return unify(variable, (Term)unSubst.get(y.getTermName()), unSubst);
         } else if (occursCheck(variable, y, unSubst)) {
             return null;
@@ -54,9 +54,9 @@ public class Unifier {
     public static boolean occursCheck(Term variable, Term y, HashMap<String, Term> unSubst) {
         if (variable.getTermName().equals(y.getTermName())) {
             return true;
-        } else if (Term.isVariable(y) && unSubst.containsKey(y.getTermName())) {
+        } else if (y.getTermType() == Term.TermType.VARIABLE && unSubst.containsKey(y.getTermName())) {
             return occursCheck(variable, (Term)unSubst.get(y.getTermName()), unSubst);
-        } else if (Term.isConstructor(y)) {
+        } else if (y.getTermType() == Term.TermType.FUNCTION) {
             AtomicBoolean result = new AtomicBoolean(false);
             y.getArguments().forEach((term) -> {
                 if (occursCheck(variable, term, unSubst)) {
@@ -67,80 +67,6 @@ public class Unifier {
             return result.get();
         } else {
             return false;
-        }
-    }
-
-    public static void showUnifier(Term term, Boolean unSubstIsVariable, HashMap<String, Term> unSubst) {
-        if (Term.isConstructor(term)) {
-            System.out.print(term.getTermName());
-            System.out.print("(");
-
-            for(int i = 0; i < term.getArguments().size(); ++i) {
-                Term arg = (Term)term.getArguments().get(i);
-                if (i != 0) {
-                    System.out.print(",");
-                }
-
-                if (unSubstIsVariable) {
-                    System.out.print(arg.getTermName());
-                } else {
-                    showUnifier(arg, false, unSubst);
-                }
-            }
-
-            System.out.print(")");
-        }
-
-        if (Term.isConstant(term)) {
-            System.out.print(term.getTermName());
-        }
-
-        if (Term.isVariable(term)) {
-            if (unSubst.containsKey(term.getTermName())) {
-                Term subTerm = (Term)unSubst.get(term.getTermName());
-                if (Term.isVariable(subTerm)) {
-                    System.out.print(subTerm.getTermName());
-                } else {
-                    showUnifier(subTerm, true, unSubst);
-                }
-            } else {
-                System.out.print(term.getTermName());
-            }
-        }
-
-    }
-
-    public static void showSubstitution(HashMap<String, Term> unSubst) {
-        unSubst.forEach((k, v) -> {
-            System.out.print(k + " := ");
-            showSub(v, unSubst);
-            System.out.println();
-        });
-    }
-
-    public static void showSub(Term term, HashMap<String, Term> unSubst) {
-        if (Term.isConstructor(term)) {
-            System.out.print(term.getTermName());
-            System.out.print("(");
-
-            for(int i = 0; i < term.getArguments().size(); ++i) {
-                Term arg = (Term)term.getArguments().get(i);
-                if (i != 0) {
-                    System.out.print(",");
-                }
-
-                showSub(arg, unSubst);
-            }
-
-            System.out.print(")");
-        }
-
-        if (Term.isConstant(term)) {
-            System.out.print(term.getTermName());
-        }
-
-        if (Term.isVariable(term)) {
-            System.out.print(term.getTermName());
         }
     }
 }
