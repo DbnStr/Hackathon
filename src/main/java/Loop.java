2aadcc1fa603e318b401fe1c4c95db3839a244b1
Rolso22 <ramagf@yandex.ru>
2021-12-15 22:45:53 +0300

    stack over flow 2

--- src/main/java/Loop.java
+++ src/main/java/Loop.java
@@ -28,6 +28,24 @@ public class Loop {
                 }
             }
         }
+//        checked.add(r.getLefTerm() + "-" + r.getRightTerm());
+//        boolean res = false;
+//        ArrayList<Rule> allRules = trs.getRules();
+//        for (Term arg : r.getRightTerm().getArguments()) {
+//            res = false;
+//            for (Rule rl : allRules) {
+//                HashMap<String, Term> unSubst = new HashMap();
+//                unSubst = Unifier.unify(arg, rl.getLefTerm(), unSubst);
+//                if (unSubst == null) {
+//                    res = true;
+//                    String rulePair = rl.getLefTerm() + "-" + rl.getRightTerm();
+//                    if (checked.contains(rulePair)) {
+//                        if (!isUnified(rl, trs, checked))
+//                            return false;
+//                    }
+//                }
+//            }
+//        }
         return res;
     }
 }
