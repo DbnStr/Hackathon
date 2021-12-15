19491e620eaf42727bdf6d92defc5b8e1d05663a
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-15 10:23:22 +0300

    lpo v0

--- src/main/java/Rule.java
+++ src/main/java/Rule.java
@@ -1,4 +1,20 @@
 public class Rule {
     private Term lefTerm;
     private Term rightTerm;
+
+    public Term getLefTerm() {
+        return lefTerm;
+    }
+
+    public Term getRightTerm() {
+        return rightTerm;
+    }
+
+    public void setLefTerm(Term lefTerm) {
+        this.lefTerm = lefTerm;
+    }
+
+    public void setRightTerm(Term rightTerm) {
+        this.rightTerm = rightTerm;
+    }
 }
--- src/main/java/TRS.java
+++ src/main/java/TRS.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TRS {
     private ArrayList<Term> variables;
@@ -11,5 +13,55 @@ public class TRS {
     public TRS(ArrayList<Term> variables, ArrayList<Rule> rules) {
         this.variables = variables;
         this.rules = rules;
+    }
+
+    public ArrayList<String> getAllFunctionsNames() {
+        Set<String> result = new HashSet<>();
+        for (Term variable : variables) {
+            result.add(variable.getTermName());
+        }
+        return new ArrayList<>(result);
+    }
+
+    private void swap(int i, int j) {
+        String tmp = variables.get(i).getTermName();
+        variables.get(i).setTermName(variables.get(j).getTermName());
+        variables.get(j).setTermName(tmp);
+    }
+
+    private ArrayList<String> nextPermutation() {
+        int n = variables.size();
+        ArrayList<String> permutation = new ArrayList<>();
+        for (Term variable : variables) {
+            permutation.add(variable.getTermName());
+        }
+        int j = n - 2;
+        while (j != -1 && permutation.get(j).compareTo(permutation.get(j + 1)) != -1) j--;
+        if (j == -1)
+            return null;
+        int k = n - 1;
+        while (permutation.get(j).compareTo(permutation.get(k)) != -1) k--;
+        swap(k, j);
+        int l = j + 1, r = n - 1;
+        while (l < r) swap(l++, r++);
+        return permutation;
+    }
+
+    public ArrayList<ArrayList<String>> getFunctionsNamesPermutations() {
+        ArrayList<ArrayList<String>> permutations = new ArrayList<>();
+        ArrayList<String> permutation = nextPermutation();
+        while (permutation != null) {
+            permutations.add(permutation);
+            permutation = nextPermutation();
+        }
+        return permutations;
+    }
+
+    public ArrayList<Rule> getRules() {
+        return rules;
+    }
+
+    public ArrayList<Term> getVariables() {
+        return variables;
     }
 }
