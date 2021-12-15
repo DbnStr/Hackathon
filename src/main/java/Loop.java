0c369d5fc971c07ee79bf62bb947c3a8f0250262
Rolso22 <ramagf@yandex.ru>
2021-12-15 22:00:57 +0300

    loopeessss

--- src/main/java/Loop.java
+++ src/main/java/Loop.java
@@ -13,24 +13,12 @@ public class Loop {
     }
 
     private static boolean isUnified(Rule r, TRS trs, ArrayList<String> checked) {
-        System.out.println(r.getLefTerm().getTermName() + " - " + r.getRightTerm().getTermName());
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         checked.add(r.getLefTerm() + "-" + r.getRightTerm());
         boolean res = false;
         ArrayList<Rule> allRules = trs.getRules();
-        for (Rule rl : trs.getRules()) {
-            System.out.println("rl1 " + rl.getLefTerm().getTermName() + " - " + rl.getRightTerm().getTermName());
-        }
         for (Rule rl : allRules) {
-            System.out.println("rl " + rl.getLefTerm().getTermName() + " - " + rl.getRightTerm().getTermName());
             HashMap<String, Term> unSubst = new HashMap();
             unSubst = Unifier.unify(r.getRightTerm(), rl.getLefTerm(), unSubst);
-            //unSubst.forEach((key, value) -> System.out.println(value.getTermName()));
-            System.out.println(unSubst);
             if (unSubst == null) {
                 res = true;
                 String rulePair = rl.getLefTerm() + "-" + rl.getRightTerm();
